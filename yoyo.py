6f908bb13e9658f320603b072e1addea60768596
gaganKUMMUR <62560026+gaganKUMMUR@users.noreply.github.com>
2023-09-20 21:12:13 +0530

    first

---
+++ yoyo.py
@@ -0,0 +1,163 @@
+from pathlib import Path
+import sys
+import torch
+import os
+import platform
+import serial
+import time
+import math
+import numpy as np
+from models.common import DetectMultiBackend
+from utils.dataloaders import LoadStreams
+from utils.general import (LOGGER, Profile, check_img_size, check_imshow,cv2,
+                           increment_path, non_max_suppression, scale_boxes,xyxy2xywh)
+from utils.plots import Annotator, colors
+from utils.torch_utils import select_device
+
+# arduino = serial.Serial(port='COM4',   baudrate=115200, timeout=.1)
+
+#################################################################
+def write_read(x):
+    arduino.write(bytes(x,   'utf-8'))
+    time.sleep(0.05)
+    data = arduino.readline()
+    return   data
+##################################################################
+
+FILE = Path(__file__).resolve()
+ROOT = FILE.parents[0]  # YOLOv5 root directory
+if str(ROOT) not in sys.path:
+    sys.path.append(str(ROOT))  # add ROOT to PATH
+ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
+def run(
+        weights=ROOT / 'best.pt',  # model path or triton URL
+        source=0,  # file/dir/URL/glob/screen/0(webcam)
+        data=ROOT / 'data.yaml',  # dataset.yaml path
+        imgsz=(640, 640),  # inference size (height, width)
+        conf_thres=0.25,  # confidence threshold
+        iou_thres=0.45,  # NMS IOU threshold
+        max_det=1,  # maximum detections per image
+        device='',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
+        view_img=False,  # show results
+        save_txt=False,  # save results to *.txt
+        save_conf=False,  # save confidences in --save-txt labels
+        save_crop=False,  # save cropped prediction boxes
+        nosave=False,  # do not save images/videos
+        classes=None,  # filter by class: --class 0, or --class 0 2 3
+        agnostic_nms=False,  # class-agnostic NMS
+        augment=False,  # augmented inference
+        visualize=False,  # visualize features
+        update=False,  # update all models
+        project=ROOT / 'runs/detect',  # save results to project/name
+        name='exp',  # save results to project/name
+        exist_ok=False,  # existing project/name ok, do not increment
+        line_thickness=3,  # bounding box thickness (pixels)
+        hide_labels=False,  # hide labels
+        hide_conf=False,  # hide confidences
+        half=False,  # use FP16 half-precision inference
+        dnn=False,  # use OpenCV DNN for ONNX inference
+        vid_stride=1,  # video frame-rate stride
+):
+    source = str(source)
+    save_img = not nosave and not source.endswith('.txt')  # save inference images
+    # is_file = Path(source).suffix[1:] in (IMG_FORMATS + VID_FORMATS)
+    # is_url = source.lower().startswith(('rtsp://', 'rtmp://', 'http://', 'https://'))
+    webcam = source.isnumeric() 
+    # if is_url and is_file:
+    #     source = check_file(source)  # download
+
+    # Directories
+    save_dir = increment_path(Path(project) / name, exist_ok=exist_ok)  # increment run
+    (save_dir / 'labels' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
+
+    # Load model
+    device = select_device(device)
+    model = DetectMultiBackend(weights, device=device, dnn=dnn, data=data, fp16=half)
+    stride, names, pt = model.stride, model.names, model.pt
+    imgsz = check_img_size(imgsz, s=stride)  # check image size
+
+    # Dataloader
+    bs = 1  # batch_size
+    if webcam:
+        view_img = check_imshow(warn=True)
+        dataset = LoadStreams(source, img_size=imgsz, stride=stride, auto=pt, vid_stride=vid_stride)
+        bs = len(dataset)
+    
+    # Run inference
+    model.warmup(imgsz=(1 if pt or model.triton else bs, 3, *imgsz))  # warmup
+    seen, windows, dt = 0, [], (Profile(), Profile(), Profile())
+    for path, im, im0s, vid_cap, s in dataset:
+        with dt[0]:
+            im = torch.from_numpy(im).to(model.device)
+            im = im.half() if model.fp16 else im.float()  # uint8 to fp16/32
+            im /= 255  # 0 - 255 to 0.0 - 1.0
+            if len(im.shape) == 3:
+                im = im[None]  # expand for batch dim
+
+        # Inference
+        with dt[1]:
+            visualize = increment_path(save_dir / Path(path).stem, mkdir=True) if visualize else False
+            pred = model(im, augment=augment, visualize=visualize)
+
+        # NMS
+        with dt[2]:
+            pred = non_max_suppression(pred, conf_thres, iou_thres, classes, agnostic_nms, max_det=max_det)
+
+        # Second-stage classifier (optional)
+        # pred = utils.general.apply_classifier(pred, classifier_model, im, im0s)
+        # Process predictions
+        det = pred[0]  # per image
+        i = 0
+        seen += 1
+        if webcam:  # batch_size >= 1
+            p, im0, frame = path[i], im0s[i].copy(), dataset.count
+            s += f'{i}: '
+        
+        p = Path(p)  # to Path
+        save_path = str(save_dir / p.name)  # im.jpg
+        txt_path = str(save_dir / 'labels' / p.stem) + ('' if dataset.mode == 'image' else f'_{frame}')  # im.txt
+        s += '%gx%g ' % im.shape[2:]  # print string
+        gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+        imc = im0.copy() if save_crop else im0  # for save_crop
+        annotator = Annotator(im0, line_width=line_thickness, example=str(names))
+        if len(det):
+            # Rescale boxes from img_size to im0 size
+            det[:, :4] = scale_boxes(im.shape[2:], det[:, :4], im0.shape).round()
+            # Print results
+            for c in det[:, 5].unique():
+                n = (det[:, 5] == c).sum()  # detections per class
+                s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
+            # Write results
+            for *xyxy, conf, cls in reversed(det):
+                if save_img or save_crop or view_img:  # Add bbox to image
+                    c = int(cls)  # integer class
+                    w,h = im0.shape[1]/2,im0.shape[0]
+                    m = (h-((xyxy[1]+xyxy[3])/2))/(((xyxy[0]+xyxy[2])/2)-w)
+                    angle = math.degrees(math.atan(m))
+                    print((xyxy[1]+xyxy[3])/(xyxy[0]+xyxy[2]))
+                    print(angle)
+                    label = None if hide_labels else (names[c] if hide_conf else f'{names[c]} {m:.2f} {angle:.2f}')
+                    
+                    # write_read(xyxy)
+                    # print(int(xyxy[0]),int(xyxy[1]),int(xyxy[2]),int(xyxy[3]))
+
+                    annotator.box_label(xyxy, label, color=colors(c, True))
+        # Stream results
+        im0 = annotator.result()
+        if view_img:
+            if platform.system() == 'Linux' and p not in windows:
+                windows.append(p)
+                cv2.namedWindow(str(p), cv2.WINDOW_NORMAL | cv2.WINDOW_KEEPRATIO)  # allow window resize (Linux)
+        
+                cv2.resizeWindow(str(p), im0.shape[1], im0.shape[0])
+            cv2.imshow(str(p), im0)
+            cv2.waitKey(1)  # 1 millisecond
+        # Save results (image with detections)
+        if save_img:
+            if dataset.mode == 'image':
+                cv2.imwrite(save_path, im0)
+               
+        # Print time (inference-only)
+
+if __name__ == '__main__':
+    run()
